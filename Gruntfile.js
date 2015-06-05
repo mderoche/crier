2fa736aa7775c4259d218a46fee9b2886ed8544e
mderoche <tangiblelime@gmail.com>
2015-06-05 14:07:01 -0400

    Test skeleton, original release, gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
       },
       dist: {
         src: [
-          'src/main.js'
+          'src/*.js'
         ],
         dest: 'dist/<%= pkg.name.replace(".js", "") %>.js'
       }
