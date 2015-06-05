8308a9fb829ab7df9e1fa8c29b02de49320f4395
mderoche <tangiblelime@gmail.com>
2015-06-05 13:59:37 -0400

    Crier - pub/sub system for JavaScript

---
+++ README.md
@@ -0,0 +1,43 @@
+# crier
+
+Pub/sub pattern for JavaScript
+
+## About
+
+A JavaScript library by Mike Deroche.
+
+See the [project homepage](http://mderoche.github.io/crier).
+
+## Installation
+
+Using Bower:
+
+    bower install crier
+
+Or grab the [source](https://github.com/mderoche/crier/dist/crier.js) ([minified](https://github.com/mderoche/crier/dist/crier.min.js)).
+
+## Usage
+
+Basic usage is as follows:
+
+    crier();
+
+For advanced usage, see the documentation.
+
+## Documentation
+
+Start with `docs/MAIN.md`.
+
+## Contributing
+
+We'll check out your contribution if you:
+
+* Provide a comprehensive suite of tests for your fork.
+* Have a clear and documented rationale for your changes.
+* Package these up in a pull request.
+
+We'll do our best to help you out with any contribution issues you may have.
+
+## License
+
+MIT. See `LICENSE.txt` in this directory.
---
+++ src/crier.js
@@ -0,0 +1,186 @@
+(function (root) {
+
+  /**
+   * Polyfill for Array.forEach() (shortened)
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+   */
+  var forEach = function (arr, callback, thisArg) {
+    var T, k;
+    var O = Object(arr);
+    var len = O.length >>> 0;
+
+    if (arguments.length > 1) { T = thisArg; }
+
+    k = 0;
+    while (k < len) {
+      var kValue;
+      if (k in O) {
+        kValue = O[k];
+        callback.call(T, kValue, k, O);
+      }
+      k++;
+    }
+  };
+
+
+  /**
+   *
+   * Channel
+   *   An object where data is published and picked up by subscribers
+   *
+   */
+  var Channel = function (name) {
+    this.name = name;
+    this.subs = [];
+  };
+
+  /**
+   * Publish to this channel
+   */
+  Channel.prototype.pub = function () {
+    var args = [].slice.call(arguments);
+    args.shift();
+    args.unshift(this.name);
+
+    forEach(this.subs, function (sub) {
+      sub.fn.apply(undefined, args);
+    }, this);
+  };
+
+  /**
+   * Subscribe to this channel
+   */
+  Channel.prototype.sub = function (sub) {
+    this.subs.push(sub);
+  };
+
+  /**
+   * Unsubscribe from this channel
+   */
+  Channel.prototype.unsub = function (fnId) {
+    var idx = this.subIndex(fnId);
+    if (!!~idx) {
+      this.subs.splice(idx, 1);
+    }
+  };
+
+  /**
+   * Clear all subscribers from this channel
+   */
+  Channel.prototype.clear = function () {
+    this.subs = [];
+  };
+
+  /**
+   * Find the location of the given subscriber in the subscriber List
+   * for this channel
+   */
+  Channel.prototype.subIndex = function (fnId) {
+    var prop = (fnId instanceof Function) ? 'fn' : 'id';
+    for (var i = 0; i < this.subs.length; i++) {
+      if (this.subs[i][prop] === fnId) {
+        return i;
+      }
+    }
+    return -1;
+  };
+
+
+  /**
+   *
+   * Crier
+   *   Manages channels and abstracts the pub/sub model
+   *
+   */
+  var Crier = function () {
+    this.chdir = {};
+  };
+
+  /**
+   * Gets references to the desired channels.  If the search argument is a
+   * regular expression, it will return references to all channels that match
+   * that regex.  If the search argument is a string, it will find or create
+   * the single channel with that name.
+   */
+  Crier.prototype.channels = function (search) {
+
+    // handle case for searching as a pattern
+    if (search instanceof RegExp) {
+      var res = [];
+
+      for (var c in this.chdir) {
+        if (search.test(c)) {
+          res.push(this.chdir[c]);
+        }
+      }
+
+      return res;
+    }
+
+    // handle case for searching for a specific channel
+    // (will also create the channel if it does not exist)
+    if (!this.chdir[search]) {
+      this.chdir[search] = new Channel(search);
+    }
+    return [this.chdir[search]];
+  };
+
+  /**
+   * Publish to channel(s)
+   */
+  Crier.prototype.pub = function (channels) {
+    var args = arguments;
+    forEach(this.channels(channels), function (channel) {
+      channel.pub.apply(channel, args);
+    }, this);
+  };
+
+  /**
+   * Subscribe to channel(s)
+   */
+  Crier.prototype.sub = function (channels, fn, id) {
+    forEach(this.channels(channels), function (channel) {
+      channel.sub({
+        id: id,
+        fn: fn
+      });
+    }, this);
+  };
+
+  /**
+   * Unsubscribe from channel(s)
+   */
+  Crier.prototype.unsub = function (channels, fnId) {
+    forEach(this.channels(channels), function (channel) {
+      channel.unsub(fnId);
+    }, this);
+  };
+
+  /**
+   * List all subscribers of channel(s)
+   */
+  Crier.prototype.subs = function (channels) {
+    var res = [];
+
+    forEach(this.channels(channels), function (channel) {
+      res.push.apply(res, channel.subs);
+    }, this);
+
+    return res;
+  };
+
+  /**
+   * Clear all subscribers from channel(s)
+   */
+  Crier.prototype.clear = function (channels) {
+    forEach(this.channels(channels), function (channel) {
+      channel.clear();
+    }, this);
+  };
+
+  // create a crier
+  var crier = new Crier();
+
+  // expose crier to the global root
+  root.Crier = crier;
+})(window);
